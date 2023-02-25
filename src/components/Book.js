b4781fc34576e5fa7c5402974c5ab511e656f9c9
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-02-25 10:00:03 +0100

    initial setup redone

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/destructuring-assignment */
 function Book(props) {
   return (
-    <div>
+    <div className="input">
       <h3>{props.title}</h3>
       <p>{props.author}</p>
       <button type="submit">Remove</button>
